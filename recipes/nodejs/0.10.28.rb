17751e83f2bcf8fb6731c0072f119e1e94a03bcd
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-05-09 01:04:56 +0200

    Added nodejs v0.10.28 recipe.

---
+++ recipes/nodejs/0.10.28.rb
@@ -0,0 +1,25 @@
+class NodeJS < FPM::Cookery::Recipe
+  description 'Evented I/O for V8 JavaScript'
+
+  name     'nodejs'
+  version  '0.10.28'
+  revision 0
+  homepage 'http://nodejs.org/'
+  source   "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
+  sha256   'abddc6441e0f208f6ed8a045e0293f713ea7f6dfb2d6a9a2024bf8b1b4617710'
+  section  'interpreters'
+
+  build_depends 'libssl-dev', 'g++', 'python'
+  depends       'openssl'
+
+  def build
+    configure :prefix => prefix
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir
+    man1.install Dir['local/share/man/man1/node.1']
+    system 'gzip', '/usr/local/share/man/man1/node.1'
+  end
+end
