4af067eab58cf4fa8fafe75b5ebc550226b84c74
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-05-18 15:00:10 +0200

    Add backup gem recipe.
    This recipe build a self-contained package with ruby 2.1.2, backups gems
    and all dependencies.

---
+++ recipes/backup/backup.rb
@@ -0,0 +1,26 @@
+class PuppetGem < FPM::Cookery::Recipe
+  description 'Backup Gem'
+
+  name 'backup'
+  version '4.0.1'
+
+  source 'nothing', :with => :noop
+
+  def build
+    gem_install name, version
+  end
+
+  def install
+    # Provide 'safe' binaries in /opt/<package>/bin like Vagrant does
+    rm_rf "#{destdir}/../bin"
+    destdir('../bin').mkdir
+    destdir('../bin').install workdir('../../shared/omnibus.bin'), 'backup'
+  end
+
+  private
+
+  def gem_install(name, version = nil)
+    v = version.nil? ? '' : "-v #{version}"
+    cleanenv_safesystem "#{destdir}/bin/gem install --no-ri --no-rdoc #{v} #{name}"
+  end
+end
---
+++ recipes/backup/recipe.rb
@@ -0,0 +1,26 @@
+class BackupOmnibus < FPM::Cookery::Recipe
+  description 'Provide an elegant DSL in Ruby for performing backups on UNIX-like systems.'
+
+  name 'backup'
+  version '1:4.0.1'
+  revision 0
+  homepage 'https://github.com/meskyanichi/backup'
+
+  vendor 'fpm'
+  maintainer '<root@bds.io>'
+
+  section 'Utilities'
+
+  source '', with: :noop
+
+  omnibus_package true
+  omnibus_dir "/opt/#{name}"
+  omnibus_recipes '../ruby/2.1.2',
+                  'backup'
+
+  def build
+  end
+
+  def install
+  end
+end
