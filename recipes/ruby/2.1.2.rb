5b6c36137c75b95f378bfc7362828a764dbaccd6
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-05-19 23:57:07 +0200

    Adapt ruby 2.1.2 recipe in order to handle Squeeze dependencies.

--- recipes/ruby/2.1.2.rb
+++ recipes/ruby/2.1.2.rb
@@ -9,16 +9,24 @@ class Ruby211 < FPM::Cookery::Recipe
   sha256 'f22a6447811a81f3c808d1c2a5ce3b5f5f0955c68c9a749182feb425589e6635'
 
   maintainer '<root@bds.io>'
-  vendor     'fpm'
+  vendor 'fpm'
 
   section 'interpreters'
 
-  build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
-                'libssl-dev', 'libyaml-dev', 'libffi-dev', 'libgdbm-dev',
-                'libncurses5-dev', 'libxml2-dev', 'libxslt1-dev', 'libc6-dev'
+  platforms [:ubuntu, :debian] do
+    build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
+                  'libssl-dev', 'libyaml-dev', 'libffi-dev', 'libgdbm-dev',
+                  'libncurses5-dev', 'libxml2-dev', 'libxslt1-dev', 'libc6-dev'
 
-  depends 'libffi5', 'libncurses5', 'libreadline6', 'libssl1.0.0',
-          'libtinfo5', 'libyaml-0-2', 'zlib1g', 'libc6', 'libgdbm3'
+    depends 'libffi5', 'libncurses5', 'libreadline6', 'libyaml-0-2', 'zlib1g',
+            'libc6', 'libgdbm3'
+
+    if Facter.value(:lsbdistcodename) == 'squeeze'
+      depends.push('libssl0.9.8')
+    else
+      depends.push('libssl1.0.0', 'libtinfo5')
+    end
+  end
 
   def build
     configure :prefix => destdir.to_s.include?('embedded') ? destdir : prefix,
