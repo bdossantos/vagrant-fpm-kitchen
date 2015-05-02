6f636a825c3cafc49e545447a2d5340088c4da32
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2015-05-02 16:26:16 +0200

    Tweak the Puppet manifest in order to handle Debian Jessie

--- manifests/requirements.pp
+++ manifests/requirements.pp
@@ -1,6 +1,20 @@
 exec { '/usr/bin/apt-get update': } ->
 
-package { ['ruby-dev', 'rubygems', 'git-core', ]:
+case $lsbdistcodename {
+  squeeze, wheezy: {
+    package { ['ruby1.9.1', 'ruby1.9.1-dev', ]:
+      ensure => installed,
+    }
+  }
+  jessie: {
+    package { ['ruby2.1:any', 'ruby2.1-dev', ]:
+      ensure => installed,
+    }
+  }
+  default: { fail "not available for ${operatingsystem}/${lsbdistcodename}"}
+}
+
+package { ['rubygems', 'git-core', 'build-essential', ]:
   ensure => installed,
 }
 
